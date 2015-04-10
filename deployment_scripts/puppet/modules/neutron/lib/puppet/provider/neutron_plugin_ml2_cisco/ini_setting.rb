ea6188c9c044fdd40e02e352e51cce4d7df9579e
Nikita Koshikov <nkoshikov@mirantis.com>
2015-04-10 14:10:06 +0300

    Puppet providers added
    Change-Id: I67ad59731df43423dd210150c2b68dfec9cff8b4

---
+++ deployment_scripts/puppet/modules/neutron/lib/puppet/provider/neutron_plugin_ml2_cisco/ini_setting.rb
@@ -0,0 +1,22 @@
+Puppet::Type.type(:neutron_plugin_ml2_cisco).provide(
+  :ini_setting,
+  :parent => Puppet::Type.type(:ini_setting).provider(:ruby)
+) do
+
+  def section
+    resource[:name].split('/', 2).first
+  end
+
+  def setting
+    resource[:name].split('/', 2).last
+  end
+
+  def separator
+    '='
+  end
+
+  def file_path
+    '/etc/neutron/plugins/ml2/ml2_conf_cisco.ini'
+  end
+
+end
